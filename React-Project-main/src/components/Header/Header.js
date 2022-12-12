18aaf105004b2a1fa754d2917520729e43c8dad1
Tünzalə <114104944+Tunzale1@users.noreply.github.com>
2022-12-13 00:25:56 +0400

    first commit

---
+++ React-Project-main/src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            
+                    <h1 className='nav-title'>
+                    <div className='salal'>
+                            M o v i e
+                    <i class="fa-solid fa-video"></i>
+                    </div>
+                    </h1>
+
+                </div>
+           
+    )
+}
+
+export default Navbar;
